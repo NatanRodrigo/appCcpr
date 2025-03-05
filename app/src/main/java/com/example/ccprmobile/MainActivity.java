5ca96261870e8dc97d5b42f240da2d8be3eee786
NatanRodrigo <natanrodrigo4@gmail.com>
2025-03-04 22:22:30 -0300

    Atualização barra de pesquisa e alteração da cor

--- app/src/main/java/com/example/ccprmobile/MainActivity.java
+++ app/src/main/java/com/example/ccprmobile/MainActivity.java
@@ -4,53 +4,89 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-
     ListView listView;
-    ArrayAdapter<String> arrayAdapter;
     String[] equipmentsList = {"Tc-105 (Quinto Piso)", "Sen-112 (Sexto Piso)", "Moinho 101 (Quarto piso)"};
-
-
+    ArrayAdapter<String> arrayAdapter;
+    SearchView searchView;
+    String searchQuery = ""; // Armazena o estado da pesquisa
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Configuração da Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Configuração da ListView
         listView = findViewById(R.id.listView);
-        arrayAdapter = new ArrayAdapter<String>( this, R.layout.list_customtext, equipmentsList);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, equipmentsList);
         listView.setAdapter(arrayAdapter);
+
+        // Restaurar estado da pesquisa
+        if (savedInstanceState != null) {
+            searchQuery = savedInstanceState.getString("searchQuery", "");
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.search);
 
-        getMenuInflater().inflate(R.menu.actionbar_menu, menu);
-        MenuItem menuItem = menu.findItem(R.id.search);
-        SearchView searchView = (SearchView) menuItem.getActionView();
-        searchView.setQueryHint("Buscar por equipamento");
+        if (searchItem != null) {
+            searchView = new SearchView(this);
+            searchItem.setActionView(searchView);
+            searchView.setQueryHint("Buscar por equipamento");
 
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    searchQuery = query;
+                    hideKeyboard();
+                    return true;
+                }
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                return false;
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    arrayAdapter.getFilter().filter(newText);
+                    return false;
+                }
+            });
+
+            // Restaurar a pesquisa após a rotação da tela
+            if (!searchQuery.isEmpty()) {
+                searchView.setQuery(searchQuery, false);
             }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                arrayAdapter.getFilter().filter(newText);
-                return false;
-            }
-        });
-
+        }
         return super.onCreateOptionsMenu(menu);
+    }
+
+    // Método para esconder o teclado após submissão da pesquisa
+    private void hideKeyboard() {
+        if (searchView != null) {
+            searchView.clearFocus();
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (imm != null) {
+                imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("searchQuery", searchQuery);
     }
 }
